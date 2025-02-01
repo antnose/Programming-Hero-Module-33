182555294c4c9a53bb9f1bb54bb860f4f7ced6c4
antnose <antnose28@gmail.com>
2025-02-01 23:25:25 +0600

    Complete 1st video of 33 module

---
+++ module33.js
@@ -0,0 +1,5 @@
+// 33-1 How does internet work, HTTP Vs HTTPs and What is an API
+
+// This method we don't use any more from now
+// const user = { id: 1, name: "Gorib", job: "actor" };
+// console.log(user);
