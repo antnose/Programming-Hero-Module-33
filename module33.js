0248deb9f6282f4b5761102a623ac0a6c3baeca1
antnose <antnose28@gmail.com>
2025-02-01 23:39:08 +0600

    complete 2nd video of 33 module

--- module33.js
+++ module33.js
@@ -3,3 +3,33 @@
 // This method we don't use any more from now
 // const user = { id: 1, name: "Gorib", job: "actor" };
 // console.log(user);
+
+// 33-2 Intro to JSON, JSON Structure, parse, stringify
+
+// const user = { id: 1, name: "Gorib", job: "actor" };
+// JavaScript Object Notation (JSON)
+// const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
+/* { id: 1, name: 'Gorib', job: 'actor' }
+ {"id":1,"name":"Gorib","job":"actor"}
+*/
+
+// const shop = {
+//   owner: "Alia",
+//   address: {
+//     street: "Kochukheet ",
+//     city: "Ranbir",
+//     country: "BD",
+//   },
+//   products: ["Laptop", "MIC", "Monitor", "keyboard"],
+//   revenue: 45000,
+//   isOpen: true,
+//   isNew: false,
+// };
+// const shopJSON = JSON.stringify(shop);
+// console.log(shop);
+// console.log(shopJSON);
+
+// const shopObject = JSON.parse(shopJSON);
+// console.log(shopObject);
